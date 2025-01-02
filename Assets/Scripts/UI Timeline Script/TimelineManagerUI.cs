c3f4b5c5246619e63566ab6053feabe1988c7374
Gege <gege@innoveam.com>
2025-01-02 10:50:51 +0700

    setup UI timeline Manager for RND

---
+++ Assets/Scripts/UI Timeline Script/TimelineManagerUI.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using Innoveam.Modules.Data;
+using SimpleJSON;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class TimelineManagerUI : MonoBehaviour
+{
+    [SerializeField] ComponentLookup componPrefab;
+    [SerializeField] private List<PlayableAssetData> storedPlayableAssets = new();
+
+    public PlayableDirector PlayableDirector;
+    public void PopulatePlayableAssets()
+    {
+        if (PlayableDirector != null)
+        {
+            TimelineAsset timelineAsset = PlayableDirector.playableAsset as TimelineAsset;
+
+            if (timelineAsset != null)
+            {
+                foreach (var track in timelineAsset.GetOutputTracks())
+                {
+                    foreach (var clip in track.GetClips())
+                    {
+                        PlayableAsset playableAsset = clip.asset as PlayableAsset;
+                        storedPlayableAssets.Add(new PlayableAssetData
+                        {
+                            playable = playableAsset,
+                            name = clip.displayName,
+                            duration = clip.duration,
+                        });
+
+                        Debug.Log($"Stored PlayableAsset: {clip.displayName}");
+                    }
+                }
+
+                Debug.Log($"Total PlayableAssets stored: {storedPlayableAssets.Count}");
+            }
+            else
+            {
+                Debug.LogError("PlayableAsset is not a TimelineAsset.");
+            }
+        }
+        else
+        {
+            Debug.LogError("PlayableDirector is not assigned.");
+        }
+    }
+
+    [ContextMenu("CreateassetsTimeline")]
+    public void CreateassetsTimeline()
+    {
+        foreach (var playAsset in storedPlayableAssets)
+        {
+            GameObject assetPlayable = Instantiate(componPrefab.Get<GameObject>("prefabObject"));
+            assetPlayable.transform.SetParent(componPrefab.Get<GameObject>("container").transform, false);
+
+            ComponentLookup compoObject = assetPlayable.GetComponent<ComponentLookup>();
+            TextMeshProUGUI textnameAssets = compoObject.Get<TextMeshProUGUI>("title");
+
+            string nameAssets = playAsset.name;
+
+            textnameAssets.text = nameAssets;
+
+            assetPlayable.gameObject.name = nameAssets;
+            assetPlayable.SetActive(true);
+        }
+    }
+}
+
+[System.Serializable]
+public struct PlayableAssetData
+{
+    public PlayableAsset playable;
+    public string name;
+    public double duration;
+}
